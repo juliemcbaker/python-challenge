0b9be92a748fd8d37973106e416f683ae6e504dc
Julie Baker <juliemcbaker@gmail.com>
2021-05-29 14:03:46 -0500

    started analysis file. Now actually attempting dictionary

--- PyBank/main.py
+++ PyBank/main.py
@@ -8,19 +8,26 @@ import csv
 # need to read in PyBank/Resources/budget_data.csv
 budget_csv = os.path.join(os.getcwd(), "PyBank", "Resources", "budget_data.csv")
 
+
 with open(budget_csv) as csv_file:            
     csv_reader = csv.reader(csv_file, delimiter = ',')
-    
+    months = -1     #starting at -1 to account for header row
+    total_change = 0
+
     for row in csv_reader:
         print(row)
-  
+        months = months + 1
+    
+        
+        #.if row != 1:
+        #.    total_change = total_change + int(row[1])
+          
   #  next(csv_reader, None)
    # for row in csv_reader:
-        
-# need to count the number of months in the dataset
-    # should be something like count rows - 1 (to account for header)
-months = row.index
-print (months)
+
+# turning data into a dictionary; from stackoverflow.com
+budget = {}
+
 
 # calculate net total amount of "Profits/Losses" for entire period
     # sum everything in column B/2
@@ -52,28 +59,29 @@ print (months)
 # Analysis should print in terminal 
 #    print("Financial Analysis")
 #    print("-------------------------")
-#    print("Total Months: " + months)
+print("Total Months: " + str(months))
 #    print("Total: $" + net_total)
 #    print("Average Change: $" + avg_change)
 #    print("Greatest Increase in Profits: " + high_time + " ($" + high_money + ")"
 #    print("Greatest Decrease in Profits: " + low_time + " ($" + low_money + ")"
 
 # Analysis should export to text (PyBank/Analysis/budget_result.txt)
-#.output_path = os.path.join(os.getcwd(), 'Analysis', 'budget_result.txt')
+output_path = os.path.join(os.getcwd(), 'PyBank', 'Analysis', 'budget_result.txt')
 
-#.with open(output_path, 'w') as text:
+# found tip about append & read at geeksforgeeks.org
+with open(output_path, 'a+') as text:
     # initializes the writerls
-   #. text_out = text.writer(text, delimiter=',')
+    text_out = text.write(output_path)
     
     #writes header
-  #.  text_out.writerow("Financial Analysis")
-  #.  text_out.writerow("-------------------------")
+    #.text_out ("Financial Analysis")
+    #.text_out ("-------------------------")
     
     #writes output & pulls from variables to input values
-  #.  text_out.writenow("Total Months: " + months)
-  #.  text_out.writenow("Total: $" + net_total)
-  #.  text_out.writenow("Average Change: $" + avg_change)
-  #.  text_out.writenow("Greatest Increase in Profits: " + high_time + " ($" + high_money + ")")
-  #.  text_out.writenow("Greatest Decrease in Profits: " + low_time + " ($" + low_money + ")")
+    #.text_out.writerow("Total Months: " + months)
+  #.  text_out.writerow("Total: $" + net_total)
+  #.  text_out.writerow("Average Change: $" + avg_change)
+  #.  text_out.writerow("Greatest Increase in Profits: " + high_time + " ($" + high_money + ")")
+  #.  text_out.writerow("Greatest Decrease in Profits: " + low_time + " ($" + low_money + ")")
                        
    
