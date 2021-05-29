649ea14742610d7a829c40baec7773bfa7661109
Julie Baker <juliemcbaker@gmail.com>
2021-05-29 17:23:18 -0500

    Going to create a parallel script that doesn't convert to dictionary to see if have better luck with for loops to iterate max & min.

--- PyBank/main.py
+++ PyBank/main.py
@@ -4,6 +4,7 @@
 
 import os
 import csv
+import operator
 
 # need to read in PyBank/Resources/budget_data.csv
 budget_csv = os.path.join(os.getcwd(), "PyBank", "Resources", "budget_data.csv")
@@ -11,10 +12,11 @@ budget_csv = os.path.join(os.getcwd(), "PyBank", "Resources", "budget_data.csv")
 budget_working = os.path.join(os.getcwd(), "PyBank", "Resources", "budget_working.csv")
 
 with open(budget_csv) as csv_file:            
-    csv_reader = csv.DictReader(csv_file, delimiter = ',')
+    csv_reader = csv.reader(csv_file, delimiter = ',')
     dummy = csv.DictReader(csv_file, delimiter = ',')
     months = 0 
-    net_total = 0    
+    net_total = 0
+
 
     for row in csv_reader:
         print(row)
@@ -22,6 +24,29 @@ with open(budget_csv) as csv_file:
         months = months + 1
         # calculates overall profit/loss for entire period
         net_total = float(row['Profit/Losses']) + net_total
+
+    # Loop is executing, but it's always iterating to the last month
+    highest_value = 0
+    lowest_value = 0
+    for i in range (months):
+        if float (row['Profit/Losses']) > highest_value:
+            highest_value = float(row['Profit/Losses'])
+            highest_month = (['Date'])
+        elif float(row['Profit/Losses']) < lowest_value:
+            lowest_value = float(row['Profit/Losses'])
+            lowest_month = (row['Date'])
+        else:
+            next
+
+    print(highest_value)
+    print(highest_month)
+    print(lowest_value)
+    #print(lowest_month)
+
+    for row in dummy:
+        newHeader = csv.writer(outfile)
+
+
 
 # starting a write file to create dictionary
 #.    with open(budget_working, mode='w') as outfile:
@@ -43,7 +68,12 @@ with open(budget_csv) as csv_file:
 #..avg_change = averages from above
 
 # Greatest increase in profits (date & amount) from entire period
-#..high_time; high_money
+# based on code found on stackoverflow.com
+#max_value = max(dummy(int('Profit/Losses')))
+# max_keys = [dict.items('Date') if dict.values(Profit/Losses) == max_value]
+#, key=Date.get)
+#print(max_value)
+#print(max_keys)
 
 # Greatest decrease in losses (date & amount) from entire period
 #..low_time; low_money
@@ -58,21 +88,22 @@ with open(budget_csv) as csv_file:
     # Greatest Decrease in Profits: Month-YEAR ($.....)
 
 # Analysis should print in terminal 
-print("Financial Analysis")
-print("-------------------------")
-print("Total Months: " + str(months))
+#.print(f'Financial Analysis')  --UNSURE WHY KEEP GETTING SYNTAX ERROR ON THIS & NEXT LINE
+#.print("-------------------------")
+print("Total Months: " + str(months)) 
 print("Total: $" + str(net_total))
 #    print("Average Change: $" + avg_change)
-#    print("Greatest Increase in Profits: " + high_time + " ($" + high_money + ")"
+print(highest_value)
+#print("Greatest Increase in Profits: " + str(highest_month) + " ($" + highest_value + ")")
 #    print("Greatest Decrease in Profits: " + low_time + " ($" + low_money + ")"
 
 # Analysis should export to text (PyBank/Analysis/budget_result.txt)
-output_path = os.path.join(os.getcwd(), 'PyBank', 'Analysis', 'budget_result.txt')
+#.output_path = os.path.join(os.getcwd(), 'PyBank', 'Analysis', 'budget_result.txt')
 
 # found tip about append & read at geeksforgeeks.org
-with open(output_path, 'a+') as text:
+#.with open(output_path, 'a+') as text:
     # initializes the writerls
-    text_out = text.write(output_path)
+ #.   text_out = text.write(output_path)
     
     #writes header
     #.text_out ("Financial Analysis")
@@ -84,5 +115,3 @@ with open(output_path, 'a+') as text:
   #.  text_out.writerow("Average Change: $" + avg_change)
   #.  text_out.writerow("Greatest Increase in Profits: " + high_time + " ($" + high_money + ")")
   #.  text_out.writerow("Greatest Decrease in Profits: " + low_time + " ($" + low_money + ")")
-                       
-   
