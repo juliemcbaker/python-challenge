94842c8b7bef56347c49cbec69cf82f19519161d
Julie Baker <juliemcbaker@gmail.com>
2021-05-30 17:38:01 -0500

    Trying to create a loop to generate a list of monthly changes--not working yet. Everything else functions correctly.

--- PyBank/main.py
+++ PyBank/main.py
@@ -11,44 +11,55 @@ budget_csv = os.path.join(os.getcwd(), "PyBank", "Resources", "budget_data.csv")
 # creating a filepath for a second file that sets as a dictionary 
 #.budget_working = os.path.join(os.getcwd(), "PyBank", "Resources", "budget_working.csv")
 
+# function to create a list of monthly changes which will be used to calculate average change
+#.change_list = []
+
+#. def monthly_change(row):
+#.    this_row = (row)
+#.    last_row = (row-1)
+#.    this_month = (this_row['Profit/Losses'])
+#.    last_month = (last_row['Profit/Losses'])
+#.    this_change = this_month - last_month
+#.    change_list.append(this_change)
+
 with open(budget_csv) as csv_file:            
     csv_reader = csv.reader(csv_file, delimiter = ',')
     dict_data = csv.DictReader(csv_file, delimiter = ',')
     months = 0 
-    net_total = 0
-
-    # this is the one converted to dictionary
+    net_total = 0 
     highest_value = 0
     lowest_value = 0
+    change_list = []
+
     for row in dict_data:
-        # counts number of months
+            # counts number of months
         months = months + 1
-        # calculates overall profit/loss for entire period
-        net_total = float(row['Profit/Losses']) + net_total
-        if float (row['Profit/Losses']) > highest_value:
-            highest_value = float(row['Profit/Losses'])
+            # calculates overall profit/loss for entire period
+        net_total = int(row['Profit/Losses']) + net_total
+
+        if row == 1:
+            start_value = int(row['Profit/Losses'])
+        else:
+            start_value = start_value    
+        
+            # identifies highest & lowest values & corresponding months
+        if int(row['Profit/Losses']) > highest_value:
+            highest_value = int(row['Profit/Losses'])
             highest_month = (row['Date'])
-        elif float(row['Profit/Losses']) < lowest_value:
-            lowest_value = float(row['Profit/Losses'])
+        elif int(row['Profit/Losses']) < lowest_value:
+            lowest_value = int(row['Profit/Losses'])
             lowest_month = (row['Date'])
         else:
             next
-
-    print(highest_value)
-    print(highest_month)
-    print(lowest_value)
-    print(lowest_month)
-
-    # this is the standard csv; trying based on stackoverflow to change the header
-#.    with open(budget_working, mode='w') as outfile:
-  #.      writer = csv.writer(outfile)
-    #.    for row in csv_reader:
-      #.      newHeader = csv.writer(outfile)
-        #.    newFN = ['Date', 'Up_Down']
-          #.  newHeader.writerow(newFN)
-         #.   print(row)
-
-#. print(outfile)
+        
+        if int(row['Profit/Losses']) != start_value:
+            this_row = (row)
+            last_row = (row-1)
+            this_month = (this_row['Profit/Losses'])
+            last_month = (last_row['Profit/Losses'])
+            this_change = this_month - last_month
+            change_list.append(this_change)
+        print(change_list)
 
 # starting a write file to create dictionary
 #.    with open(budget_working, mode='w') as outfile:
@@ -69,29 +80,10 @@ with open(budget_csv) as csv_file:
 # Pt2: find the average of those changes
 #..avg_change = averages from above
 
-# Greatest increase in profits (date & amount) from entire period
-# based on code found on stackoverflow.com
-#max_value = max(dummy('Profit/Losses'))
-# max_keys = [dict.items('Date') if dict.values(Profit/Losses) == max_value]
-#, key=Date.get)
-#print(max_value)
-#print(max_keys)
-
-# Greatest decrease in losses (date & amount) from entire period
-#..low_time; low_money
-
-# Analysis should be
-    # Financial Analysis
-    # ------------------------
-    # Total Months: xxx
-    # Total: $xxxxxxx
-    # Average Change: $
-    # Greatest Increase in Profits: Month-YEAR ($.....)
-    # Greatest Decrease in Profits: Month-YEAR ($.....)
 
 # Analysis should print in terminal 
-#.print(f'Financial Analysis')  --UNSURE WHY KEEP GETTING SYNTAX ERROR ON THIS & NEXT LINE
-#.print("-------------------------")
+print(f'Financial Analysis')
+print("-------------------------")
 print("Total Months: " + str(months)) 
 print("Total: $" + str(net_total))
 #    print("Average Change: $" + avg_change)
